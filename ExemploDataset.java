fc9e91d2969c5e58055aff4248abc14c05ca338e
Thiago <91092364+thag0@users.noreply.github.com>
2023-07-30 20:41:53 -0300

    Add files via upload

--- ExemploDataset.java
+++ ExemploDataset.java
@@ -9,27 +9,29 @@ public class ExemploDataset{
       GerenciadorDados gd = new GerenciadorDados();
       ConversorDados cd = new ConversorDados();
 
-      //manusear dados
+      // manusear dados
+      // tratamento específico para os dados do iris com adaptação
+      // para usar a tangente hiperbolica como função de ativação
       ArrayList<String[]> iris = gd.lerCsv("./dados/datasets-maiores/iris.csv");
       gd.removerLinhaDados(iris, 0);
-      gd.editarValorDados(iris, (iris.get(0).length-1), "Iris-setosa", "-1");
-      gd.editarValorDados(iris, (iris.get(0).length-1), "Iris-versicolor", "0");
-      gd.editarValorDados(iris, (iris.get(0).length-1), "Iris-virginica", "1");
+      int ultimoIndice = (iris.get(0).length-1);
+      gd.editarValorDados(iris, ultimoIndice, "Iris-setosa", "-1");
+      gd.editarValorDados(iris, ultimoIndice, "Iris-versicolor", "0");
+      gd.editarValorDados(iris, ultimoIndice, "Iris-virginica", "1");
       
 
-      //transformar em treino e teste
+      // converter os dados da estrutura de texto em valores numéricos para o 
+      // treino da rede neural.
+      // separar em treino e teste para evitar overfitting
       double[][] dados = cd.listaParaDadosDouble(iris);
-
-      double[][][] treinoTeste;
-      double[][] treino, teste;
-      treinoTeste = gd.separarTreinoTeste(dados, 0.25f);
-      treino = treinoTeste[0];
-      teste = treinoTeste[1];
+      double[][][] treinoTeste = gd.separarTreinoTeste(dados, 0.25f);
+      double[][] treino = treinoTeste[0];
+      double[][] teste = treinoTeste[1];
       gd.embaralharDados(treino);
       gd.embaralharDados(teste);
 
       double[][] treinoX, treinoY, testeX, testeY;
-      int colunasDados = 4;// quantidade de características dos dados (features)
+      int colunasDados = 4;// quantidade de características dos dados (feature)
       int colunasClasses = 1;// quantidade de classificações dos dados (class)
 
       treinoX = gd.separarDadosEntrada(treino, colunasDados);
@@ -37,15 +39,18 @@ public class ExemploDataset{
       testeX = gd.separarDadosEntrada(teste, colunasDados);
       testeY = gd.separarDadosSaida(teste, colunasClasses);
 
-      //treinando rede neural
+      // criando, configurando e treinando a rede neural.
+      // os valores de configuração não devem ser tomados como regra e 
+      // devem se adaptar ao problema e os dados apresentados.
       int[] arq = {colunasDados, 6, 4, colunasClasses};
       RedeNeural rede = new RedeNeural(arq);
       rede.configurarTaxaAprendizagem(0.001);
       rede.configurarMomentum(0.9);
       rede.compilar();
       rede.configurarFuncaoAtivacao(3);
-
       rede.treinoGradienteEstocastico(treinoX, treinoY, 1_000);
+
+      // avaliando os resultados da rede neural
       double precisao = rede.calcularPrecisao(testeX, testeY);
       double custo = rede.funcaoDeCusto(testeX, testeY);
       System.out.println("Custo: " + custo);
